3d6d78822165aee0bb7177728b3af4cd0e9c274c
khanaltamash <59774724+khanaltamash@users.noreply.github.com>
2020-10-04 01:01:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import time
+import numpy as np
+import cv2
+
+car_classifier = cv2.CascadeClassifier('car.xml')
+cap = cv2.VideoCapture('cars.mp4')
+while cap.isOpened():
+
+    time.sleep(.10)
+
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    cars = car_classifier.detectMultiScale(gray, 1.1, 2)
+
+    for (x, y, w, h) in cars:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+        cv2.imshow('Cars', frame)
+    if cv2.waitKey(1) == 13:
+        break
+cap.release()
+cv2.destroyAllWindows()
